8c5c6e3f8efa0649dbfb1303c82cfa728f843745
Lupy29 <sovlkids.est08@gmail.com>
2022-01-23 20:09:57 -0800

    iiiii

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  
+  return (
+      < div className='navbar'>
+          <div className='container'>
+              <div className='w-3/12'></div>
+              <div className='logo text-center uppercase text-2xl'>
+                   nciii
+              </div>
+              <div className='w-6/12'></div>
+              <div className='nav'>
+
+              </div>
+          </div>
+      </div>
+      
+  )
 }
 
 export default App;
